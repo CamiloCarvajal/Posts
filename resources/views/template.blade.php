40760785119deadc3f87da03c8a5e5239f70d756
camilo carvajal <camilocc597@gmail.com>
2023-01-15 22:59:26 -0500

    agregando authenticacion al proyecto

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -8,6 +8,12 @@
     <p>
         <a href="\">Home</a>
         <a href="\blog">Blog</a>
+        
+        @auth
+            <a href="{{route('dashboard')}}">Dashboard</a>
+        @else
+            <a href="{{route('login')}}">Login</a>
+        @endauth
     </p>
     <hr>
     
