3fbecda6a6676573f0760c6eae6201435fd2d72a
camilo carvajal <camilocc597@gmail.com>
2023-01-16 20:34:04 -0500

    adding a index post route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -31,5 +32,7 @@ Route::controller(PageController::class)->group(function () {
     Route::get('blog', 'blog')->name('blog');
     Route::get('blog/{post:slug}', 'post')->name('post');
 });
+
+Route::resource('posts', PostController::class)->except(['show']);
 
 require __DIR__.'/auth.php';
